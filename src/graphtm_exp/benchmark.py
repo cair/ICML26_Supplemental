6a0c685be1da790a16681df309f6f5dc862e43d6
Mayur Kishor Shende <mayurks@uia.no>
2025-10-27 13:55:00 +0000

    Initial BM impl

--- src/graphtm_exp/benchmark.py
+++ src/graphtm_exp/benchmark.py
@@ -1,11 +1,14 @@
+# WARN: INCOMPLETE
 import numpy as np
 from GraphTsetlinMachine.tm import MultiClassGraphTsetlinMachine
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from tqdm import tqdm
+import pandas as pd
 
 from graphtm_exp.graph import Graphs
-from .Timer import Timer
+from .timer import Timer
+from datetime import datetime
 
 
 class Benchmark:
@@ -45,6 +48,25 @@ class Benchmark:
             self.Y_train = Y[train_idx]
             self.graphs_train = graphs[train_idx]
 
+        self.fname = f"{save_dir}/bm_res_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"  # TODO: Create empty file
+
+        self.splits = self.create_splits()
+
+    def create_splits(self) -> dict[str, tuple[np.ndarray, np.ndarray]]:
+        """
+        Create Splits. Return a dict [split_name: (train_idx, val_idx)]
+        """
+        # Maybe this can be done in __init__?
+
+        self.kf = StratifiedKFold(n_splits=5, random_state=2)
+        kf_splits = self.kf.split(np.zeros(self.Y_train.shape[0]), self.Y_train)
+
+        splits = {}
+        for fold, (train_idx, val_idx) in enumerate(kf_splits):
+            splits[f"val_{fold}"] = (train_idx, val_idx)
+
+        return splits
+
     def metrics(self, ytrue, ypred):
         precision, recall, f1, _ = precision_recall_fscore_support(ytrue, ypred, average="weighted", zero_division=0)
         accuracy = accuracy_score(ytrue, ypred)
@@ -57,36 +79,55 @@ class Benchmark:
         y_train: np.ndarray,
         graphs_val: Graphs,
         y_val: np.ndarray,
-    ):
-        history = {}
+    ) -> dict[int, dict]:
+        history: dict[int, dict] = {}
         for epoch in (pbar := tqdm(range(self.epochs), desc="GTM Fit", leave=False, dynamic_ncols=True)):
-            with Timer() as fit_timer:
+            with (fit_timer := Timer()):
                 tm.fit(graphs_train, y_train, epochs=1, incremental=True)
 
-            with Timer() as pred_timer:
+            with (pred_timer := Timer()):
                 y_val_pred = tm.predict(graphs_val)
 
-            metrics = self.metrics(y_val, y_val_pred)
-            metrics = {**metrics, "fit_time": fit_timer.elapsed(), "pred_time": pred_timer.elapsed()}
+            y_train_pred = tm.predict(graphs_train)
+
+            val_metrics = self.metrics(y_val, y_val_pred)
+            train_metrics = self.metrics(y_train, y_train_pred)
+            metrics = {
+                **{f"val_{k}": v for k, v in val_metrics.items()},
+                **{f"train_{k}": v for k, v in train_metrics.items()},
+                "fit_time": fit_timer.elapsed(),
+                "pred_time": pred_timer.elapsed(),
+            }
             history[epoch] = metrics
             pbar.set_postfix_str(f"Acc: {metrics['accuracy']:.4f}")
 
         return history
 
     def save_results(self, history: dict[int, dict]):
-        # TODO: Save results to a file in self.save_dir
-        pass
-
-    def create_splits(self):
-        # TODO: Create splits: Should return indices for each split
-        # Maybe this can be done in __init__?
-        pass
+        # TODO: Most likely wrong
+        df = pd.DataFrame.from_dict(history, orient="index")
+        df.to_csv(self.fname, mode="a")
 
     def run(self):
-        # TODO: Run the benchmark
-        # 1. Crete dataset splits
-        # 2. For each split,
-        #   Create a new model, and fit with the train/val split
-        #   test dataset?
-        #   Save the results
-        pass
+        # Go through each split
+        for split_name, (train_idx, val_idx) in self.splits.items():
+            print(f"=============Running split: {split_name}=============")
+            graphs_train_split = self.graphs_train[train_idx]
+            y_train_split = self.Y_train[train_idx]
+            graphs_val_split = self.graphs_train[val_idx]
+            y_val_split = self.Y_train[val_idx]
+
+            # GTM
+            gtm = MultiClassGraphTsetlinMachine(**self.gtm_args)
+            hist = self.fit_gtm(gtm, graphs_train_split, y_train_split, graphs_val_split, y_val_split)
+
+            # Save Results
+            self.save_results(hist)
+
+        # Finally test set
+        print("=============Final evaluation on test set=============")
+        gtm = MultiClassGraphTsetlinMachine(**self.gtm_args)
+        hist = self.fit_gtm(gtm, self.graphs_train, self.Y_train, self.graphs_test, self.Y_test)
+        self.save_results(hist)
+
+        print(f"We are done! Results saved to {self.fname}.")
